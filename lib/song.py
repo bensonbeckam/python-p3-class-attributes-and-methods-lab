e8d88b97c2883f16b54ee7edb3d6d40fa3d710d6
Beckham <bensonbeckham98@gmail.com>
2025-09-01 18:54:32 +0300

    done

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,34 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update song count
+        Song.count += 1
+
+        # Add genre to list (no duplicates)
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # Add artist to list (no duplicates)
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # Update genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # Update artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
